573dc6e28f2c79a2abcd8bf444af93d4465e1be8
= <achten.rafael16@gmail.com>
2023-10-16 17:54:03 -0300

    integração com a api, api local

---
+++ src/app/useCases/products/createProducts.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { Product } from "../../models/Product";
+
+export async function createProducts(req: Request, res: Response) {
+  const imagePath = req.file?.filename;
+  const { name, description, price, category, ingredients } = req.body;
+
+  try {
+    const product = await Product.create({
+      name,
+      description,
+      price: Number(price),
+      category,
+      ingredients: ingredients ? JSON.parse(ingredients) : [],
+      imagePath,
+    });
+
+    res.status(201).json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Product creation failed" });
+  }
+}
---
+++ src/router.ts
@@ -0,0 +1,60 @@
+import { Router } from "express";
+import path from "node:path";
+import multer from "multer";
+
+import { listCategories } from "./app/useCases/categories/listCategories";
+import { createCategory } from "./app/useCases/categories/createCategories";
+import { listProducts } from "./app/useCases/products/listProducts";
+import { createProducts } from "./app/useCases/products/createProducts";
+import { listProductsByCategory } from "./app/useCases/categories/listProductsByCategory";
+import { listsOrders } from "./app/useCases/orders/listOrders";
+import { createOrder } from "./app/useCases/orders/createOrders";
+import { changeOrderStatus } from "./app/useCases/orders/changeOrderStatus";
+import { cancelOrder } from "./app/useCases/orders/cancelOrder";
+
+export const router = Router();
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, callback) {
+      callback(null, path.resolve(__dirname, "..", "uploads"));
+    },
+    filename(req, file, callback) {
+      callback(null, `${Date.now()}--${file.originalname}`);
+    },
+  }),
+});
+
+// list categories
+router.get("/categories", listCategories);
+
+// create category
+router.post("/categories", createCategory);
+
+// list products
+
+router.get("/products", listProducts);
+
+// create product
+
+router.post("/products", upload.single("image"), createProducts);
+
+// get products by category
+
+router.get("/categories/:categoryId/products", listProductsByCategory);
+
+// list orders
+
+router.get("/orders", listsOrders);
+
+// create order
+
+router.post("/orders", createOrder);
+
+// change order status
+
+router.patch("/orders/:orderId", changeOrderStatus);
+
+// delete/cancel order
+
+router.delete("/orders/:orderId", cancelOrder);

275ef22a1a265be8f02dbc75161620a17874cf7a
= <achten.rafael16@gmail.com>
2023-10-16 19:07:11 -0300

    inicializando api

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,18 @@ mongoose
   .then(() => {
     const port = 5001;
     const app = express();
+
+    app.use((req, res, next) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Methods", "*");
+      res.setHeader("Access-Control-Allow-Headers", "*");
+
+      next();
+    });
+
     app.use(
       "/uploads",
-      express.static(path.resolve(__dirname, "..", "uploads"))
+      express.static(path.resolve(path.resolve(__dirname, "..", "uploads")))
     );
     app.use(express.json());
     app.use(router);
